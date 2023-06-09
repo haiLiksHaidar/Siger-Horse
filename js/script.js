db154977cd8d16c32b39703543e23d07bacb75fb
haiLiks Haidar <hailikshaidar@gmail.com>
2023-06-09 10:36:39 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -30,4 +30,45 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
-     
+// toggle class active-shoppingCart
+const shoppingCart = document.querySelector(".shopping-cart");
+// ketika shoppingCartButton di-klik
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+// klik di luar sidebar untuk menghilangkan shopping-cart
+const shoppingButton = document.querySelector("#shopping-cart-button");
+const shoppingcartSide = document.querySelector(".shopping-cart");
+
+document.addEventListener("click", function (e) {
+  if (
+    !shoppingButton.contains(e.target) &&
+    !shoppingcartSide.contains(e.target)
+  ) {
+    shoppingcartSide.classList.remove("active");
+  }
+});
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombil close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+// klik luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
